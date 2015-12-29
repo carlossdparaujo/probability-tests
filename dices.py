ec6cd3b007962c173d776979bc2a37c7e8b71443
Carlos Sergio de Paiva Araujo <carlossdparaujo@gmail.com>
2015-12-29 15:32:06 -0200

    Creating classes for the dices.

---
+++ dices.py
@@ -0,0 +1,22 @@
+from random import randint
+
+class Dice:
+	def __init__(self, number_of_sides):
+		self.number_of_sides = number_of_sides
+
+	def roll_dice(self):
+		return randint(1, self.number_of_sides)
+
+class DishonestDice:
+	def __init__(self, number_of_sides, dishonesty, dishonest_side):
+		self.number_of_sides = number_of_sides
+		self.dishonesty = dishonesty
+		self.dishonest_side = dishonest_side
+
+	def roll_dice(self):
+		dice_roll = randint(1, self.number_of_sides)
+
+		if dice_roll > self.number_of_sides*self.dishonesty:
+			return dice_roll
+		else:
+			return self.dishonest_side
